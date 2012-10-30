c730e18200b9b176dac37bb82a1dc7b19ff7e71c
Administrator <Administrator@AdministratortekiiMac-2.local>
2012-10-30 13:34:50 +0800

    Initial Commit

---
+++ 汉字转拼音/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  汉字转拼音
+//
+//  Created by Administrator on 12-10-30.
+//  Copyright (c) 2012年 刘鹏卫. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
